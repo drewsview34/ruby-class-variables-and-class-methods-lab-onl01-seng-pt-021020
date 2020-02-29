c94dee8727a131c456d51fb20f25cbdc77fa7656
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-29 18:05:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.map {|x| x unless @@genres.include?(x)}
+  end 
+  
+  def self.artist 
+    
+  end
+  
+end
