8ad7bba7c0c2752ae3c6cf58f047d2f2f40bc84e
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-29 18:19:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,12 @@ class Song
   
   def self.genre_count
     genre_count = {}
-      @@artists.each do |artist|
+    
+  end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1 
       else
@@ -37,9 +42,4 @@ class Song
     end
     artist_count
   end
-  
-  def self.artist_count
-    
-  end
-  
 end
